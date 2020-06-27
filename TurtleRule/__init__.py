8abb8e8b65f73445176a7cb7750d43e480fbb44e
Fengkie Junis <fengkiejunis@yahoo.co.id>
2020-06-27 15:31:23 +0700

    Update __init__.py

--- TurtleRule/__init__.py
+++ TurtleRule/__init__.py
@@ -1,7 +1,7 @@
 """
 The Original Turtle Trading Rules Strategy
 2020-06-20 original implementation - FengkieJ (fengkiejunis@gmail.com)
-2020-06-27 rev 1 - add System 1 & System 2 as strategy parameters
+2020-06-27 rev 1. add System 1 & System 2 as strategy parameters - FengkieJ (fengkiejunis@gmail.com)
 
 Turtle strategy is one of classic trend-following system that has clear defined trading rules. The system was taught by Richard Dennis 
 and William Eckhardt in 1983. This version implements the system described by Curtis Faith, author of bestselling book Way of the Turtle.
